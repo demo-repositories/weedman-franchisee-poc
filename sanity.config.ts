591ada901ade9df1e67baea01cf74e3c5dc74d88
Adam Gray <adam.gray@sanity.io>
2025-03-13 15:48:22 -0500

    cleanup

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,78 +4,14 @@ import {visionTool} from '@sanity/vision'
 import {franchiseeOwnerSchemaTypes, globalAdminSchemaTypes} from './schemaTypes'
 import {createRoleOnPublish} from './actions/createRoleOnPublish'
 import type {DocumentActionComponent, Role} from 'sanity'
-import {createClient} from '@sanity/client'
 
-const franchisees = [
-  {
-    name: 'weedman-austin',
-    title: 'Weedman Austin',
-  },
-  {
-    name: 'weedman-toronto',
-    title: 'Weedman Toronto',
-  }
-]
-
-const client = createClient({
-  projectId: 'b7tq4qrc',
-  dataset: 'production',
-  useCdn: false,
-  apiVersion: '2023-05-03',
-})
-
-const getFranchiseeStructure = (f: any) => async (S: any) => {
-  // Fetch the franchisee document by name
-  const franchisee = await client.fetch(
-    `*[_type == "franchisee" && name == $name][0]`,
-    { name: f.name }
-  )
-
-  const franchiseeTitle = franchisee.title
-
-  if (!franchisee?._id) {
-    return S.list()
-      .title(franchiseeTitle)
-      .items([
-        S.listItem()
-          .id("no-store-information")
-          .title(`No Store Information found for ${franchiseeTitle}`)
-          .icon(() => '⚠️')
-          .child(
-            S.component()
-              .id("no-store-information-component")
-              .component(() => {
-                return {
-                  component: () => (
-                    `No Store Information found for ${franchiseeTitle}`
-                  ),
-                  canHandleIntent: () => false
-                }
-              })
-          )
-      ])
-  }
-
-  const franchiseeId = franchisee._id
-  
-  return S.list()
-    .id("store-information")
-    .title(franchiseeTitle)
-    .items([
-      S.listItem()
-        .title('Store Information')
-        .schemaType('franchisee')
-        .child(
-          S.document().documentId(franchiseeId).schemaType("franchisee")
-        )
-    ])
-}
+const PROJECT_ID = 'b7tq4qrc'
 
 export default defineConfig([
   {
     name: 'default',
     title: 'Weedman Global Admin',
-    projectId: 'b7tq4qrc',
+    projectId: PROJECT_ID,
     dataset: 'production',
     basePath: "/admin",
     plugins: [structureTool(), visionTool()],
@@ -99,7 +35,7 @@ export default defineConfig([
   {
     name: 'franchisee',
     title: 'Weedman Franchisee',
-    projectId: 'b7tq4qrc',
+    projectId: PROJECT_ID,
     dataset: 'production',
     basePath: "/franchisee",
     plugins: [structureTool({
